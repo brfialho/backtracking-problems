14bf52785feffba065b4a6b2ec8b839f22144427
Bruno <brunolegal3@gmail.com>
2026-04-01 18:00:53 -0300

    rip: almost there? maybe

--- rip.c
+++ rip.c
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 16:04:17 by brfialho          #+#    #+#             */
-/*   Updated: 2026/04/01 17:55:11 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/04/01 18:00:04 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,22 +32,6 @@ int	count_pairs(char *s)
 	return (pairs);
 }
 
-char	count_min_parenthesis(char *s)
-{
-	int open = 0;
-	int close = 0;
-
-	while (*s)
-	{
-		if (*s == '(')
-			open++;
-		else if (*s == ')')
-			close++;
-		s++;
-	}
-	return ((open <= close) * open + (close < open) * close);
-}
-
 char	is_balanced(char *s)
 {
 	int		open;
@@ -68,10 +52,15 @@ char	is_balanced(char *s)
 	return ((open - close) == 0);
 }
 
-void	solver(char *s, int min, int index)
+void	solver(char *s, int pairs, int index)
 {
 	if (!s[index])
 		return ;
+	if (is_balanced(s))
+	{
+		puts(s);
+		return ;
+	}
 
 	int		i;
 	char	c;
@@ -81,12 +70,10 @@ void	solver(char *s, int min, int index)
 	{
 		c = s[i];
 		s[i] = ' ';
-		if (is_balanced(s))
-			puts(s);
-		solver(s, min, index + 1);
+		if (count_pairs(s) >= pairs)
+			solver(s, pairs, index + 1);
 		s[i] = c;
 	}
-	(void)min;
 }
 
 int main(int argc, char **argv)
@@ -94,15 +81,14 @@ int main(int argc, char **argv)
 	if (argc != 2 || !argv[1][0])
 		return (1);
 
-	// if (is_balanced(argv[1]))
-	// 	return (puts(argv[1]));
+	if (is_balanced(argv[1]))
+		return (puts(argv[1]));
 
 	char	s[10000] = {0};
 	int i = -1;
 	while (argv[1][++i])
 		s[i] = argv[1][i];
 
-	printf("PAIRS: %d\n", count_pairs(s));
-	// printf("MIN: %d\n", count_min_parenthesis(s));
-	// solver(s, count_min_parenthesis(s), 0);
+	// printf("PAIRS: %d\n", count_pairs(s));
+	solver(s, count_pairs(s), 0);
 }
